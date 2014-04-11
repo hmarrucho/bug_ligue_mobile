b6a07f2e381c5bd7cb1fd16e3b1cd07d9964140d
thibaut <thibaut@plugmystore.com>
2014-04-11 16:35:56 +0200

    Prio

--- mobile/util/fonctions_mobile.js
+++ mobile/util/fonctions_mobile.js
@@ -43,12 +43,12 @@ $(document).ready(function(){
                     if(data['status'] == "Ouvert"){
                         $("#clore_bouton").html("<a id='cloreBug' href='#ticket_clore' data-transition='flip' onclick=\"clore()\">Clore</a>");
                         $("#assign_bouton").html("<a id='assignBug' href='#ticket_assign' data-transition='flip' onClick='assign_Bug_R()'>Assigner</a>");
-                        $("#prio_bouton").html("<a id='prioBug' href='#ticket_prio' data-transition='flip' onClick='prio()'>Priorité</a>");
+                        $("#prio_bouton").html("<a id='prioBug' href='#popupMenu' data-rel='popup' data-role='button' data-inline='true' data-transition='slideup' data-theme='e'>Priorité</a><div data-role='popup' id='popupMenu' data-theme='d'><ul data-role='listview' data-inset='true' style='min-width:210px;' data-theme='d'><li><a href='index.php?uc=dash&prio=haut&bug="+identifiant+"'>Haut</a></li><li><a href='index.php?uc=dash&prio=normal&bug="+identifiant+"'>Normal</a></li><li><a href='index.php?uc=dash&prio=haut&bug="+identifiant+"'>Bas</a></li></ul></div>");
                     }
                 }
                 $("#id_tick").html(identifiant);
                 $("#contenu_clore").html("<form name='clos_bug' method='POST' action='index.php?uc=dash'><p><label for='note'>Note : </label></p><p><textarea name='note' rows='8' cols='50' placeholder='Ecrivez ici...'></textarea></p><input type='hidden' name='bug' value='"+identifiant+"'><center><p><input type='submit' class='button' value='Valider' name='valider'></p></center></form>");
-                $("#contenu_prio").html("<form name='prio_bug' method='POST' action='index.php?uc=dash' ><fieldset data-role='controlgroup'><legend>Niveau de priorité</legend><input type='radio' name='prio' id='haut' value='Haut' /><label for='haut'>Haut</label><input type='radio' name='prio' id='normal' value='Normal' /><label for='normal'>Normal</label><input type='radio' name='prio' id='bas' value='Bas' /><label for='bas'>Bas</label><input type='hidden' name='bug' value='"+identifiant+"'><p><input type='submit' class='button' value='Valider' name='valider'></p></fieldset></form>");
+                $("#contenu_prio").html("<form name='prio_bug' method='POST' action='index.php?uc=dash'><fieldset data-role='controlgroup'><legend>Niveau de priorité</legend><input type='radio' name='prio' id='haut' value='Haut' /><label for='haut'>Haut</label><input type='radio' name='prio' id='normal' value='Normal' /><label for='normal'>Normal</label><input type='radio' name='prio' id='bas' value='Bas' /><label for='bas'>Bas</label><input type='hidden' name='bug' value='"+identifiant+"'><p><input type='submit' class='button' value='Valider' name='valider'></p></fieldset></form>");
                 // on active le clic sur le lien invisible pour déclencher le dialog
                 $('#lnkDialog').click();
                 // Autre façon de changer la page à la volée
